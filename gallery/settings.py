0ea05fe01e0a721acb9abdbc1c47e837d4d7236e
nancy.kigotho@student.moringaschool.com <nancykigotho14@gmail.com>
2021-10-09 02:53:40 +0300

    update the settings.py

--- gallery/settings.py
+++ gallery/settings.py
@@ -160,8 +160,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
